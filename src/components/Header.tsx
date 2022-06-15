b2dcc22917d5150e0e88c9bf50e15c6570c58778
ccrsxx <aminrisal@gmail.com>
2022-06-15 21:15:31 +0700

    update header for mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,13 +6,13 @@ export function Header({ className }: HeaderProps) {
   return (
     <header
       className={`${className} relative top-28 animate-fade-up text-5xl transition-all duration-1000 
-                  ease-in-out sm:top-36 sm:text-6xl `}
+                  ease-in-out sm:top-36 sm:text-6xl`}
     >
       <h1
-        className='bg-gradient-to-r from-orange-400 to-pink-400
-                   bg-clip-text font-bold text-transparent'
+        className='flex flex-col gap-2 bg-gradient-to-r from-orange-400
+                   to-pink-400 bg-clip-text font-bold text-transparent sm:block'
       >
-        WAIFU MEMORY CARD
+        <span>WAIFU</span> MEMORY CARD
       </h1>
     </header>
   );
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,7 +6,7 @@ export function Main({ children }: MainProps) {
   return (
     <main
       className='flex w-full max-w-7xl flex-1 flex-col items-center
-                 justify-center gap-8 rounded-lg p-2 py-4 sm:p-8'
+                 justify-center gap-8 rounded-lg p-2 py-6 sm:p-8'
     >
       {children}
     </main>
